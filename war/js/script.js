34b623dd1f4769ccee26c6b32ae2d4edf094ed10
Swallow <swallow.cc@gmail.com>
2014-05-13 12:58:52 +0800

    fix css id program

--- war/js/script.js
+++ war/js/script.js
@@ -16,7 +16,6 @@ function removeIt(id, str2) {
 	$('#showArea2').empty();
 }
 function userList(voter) {
-//	var voter = response[id].voter == null ? "" : response[id].voter ;
 	$('#userList').show();
 	$('#userList').html(voter);
 	$('#userList').append("<hr/><center><input type='button' value='close' onclick='hide()' /></center>");
@@ -26,7 +25,7 @@ function hide() {
 }
 $(function(){
 	$("#copys").zclip({
-	      path:"ZeroClipboard.swf",
+	      path:"js/ZeroClipboard.swf",
 	      copy:function(){return $("input#dynamic").val();}
 	});
 	$('#userList').draggable();
